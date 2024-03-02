3637589467f679e9f7cf4c2ddf46ee1a17d465a1
thiagocmachado@yahoo.com.br <thiagocmachado@yahoo.com.br>
2024-03-02 10:29:09 +0100

    static

--- dineapp/urls.py
+++ dineapp/urls.py
@@ -9,6 +9,5 @@ urlpatterns = [
     path('dashboard/', include("caixa.urls")),
     path('backup/', include("backup_app.urls")),
 
-]# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
